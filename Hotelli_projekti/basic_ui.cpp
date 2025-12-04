fe730ae8f7f75a872a9603e138ae93ed48e52b49
zharzh0 <viianen.ville@gmail.com>
2025-12-04 16:52:05 +0200

    update reservationUI to view available rooms

--- Hotelli_projekti/basic_ui.cpp
+++ Hotelli_projekti/basic_ui.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int basicUI() {
 
 	int selection;
-	cout << "Tervetuloa hotelliin \n";
 	cout << "1. Varaa hotellihuone\n" ;
 	cout << "2. Tarkastele varaustasi\n";
 	cout << "3. Peru varaus\n";
-	cout << "4. Sulje ohjelma\n";
-
+	cout << "4. Vapaiden huoneiden maara\n";
+	cout << "5. Sulje ohjelma\n";
+	
 	cin >> selection;
 	return selection;
 }
